29d4cb81dcdd8eda02527715b92e47e76068be66
Yaroslav <yaroslavpopovich04@gmail.com>
2019-11-04 17:15:15 +0200

    +deleted

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 before = cv2.imread('1.png')
 after = cv2.imread('2.png')
 
+result_after = after.copy()
+
 H = before.shape[0]
 W = before.shape[1]
 
@@ -24,7 +26,7 @@ def check(y1, x1, y2, x2):
     return True
 
 def bfs(y, x, maxSize):
-    print('bfs!')
+    # print('bfs!')
     global H, W, before, after, used
     leftAns = 1e8
     rightAns = -1e8
@@ -37,8 +39,8 @@ def bfs(y, x, maxSize):
     q.append((y, x))
     while(len(q) != 0):
         # used[q[0][0]][q[0][1]] = True
-        if col % 10000 == 0:
-            print(col)
+        # if col % 10000 == 0:
+        #     print(col)
         leftAns = min(q[0][1], leftAns)
         rightAns = max(q[0][1], rightAns)
         upperAns = min(q[0][0], upperAns)
@@ -62,12 +64,13 @@ def bfs(y, x, maxSize):
             col+= 1
 
         q.pop(0)
-    print('End of loop')
+    # print('End of loop')
     if col > maxSize:
         return (-1, -1, -1, -1, -1)
     return (leftAns, rightAns, upperAns, bottomAns, col)
 
 print(f'{H} x {W}')
+print('loading...')
 for i in range(0, H):
     for j in range(0, W):
         for g in range(1, 11):
@@ -81,7 +84,8 @@ for i in range(0, H):
             # if i > 2:
             #     cv2.imwrite(f'{i}.png', sample)
 
-            after = cv2.rectangle(after, (leftAns, upperAns), (rightAns, bottomAns), (255, 0, 0), 1)
+            # cv2.rectangle(result_after, (leftAns, upperAns), (rightAns, bottomAns), (255, 0, 0), 1)
+            # before = cv2.rectangle(before, (leftAns, upperAns), (rightAns, bottomAns), (255, 0, 0), 1)
 
             img_grey = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
             template = cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)
@@ -90,13 +94,20 @@ for i in range(0, H):
             res = cv2.matchTemplate(img_grey, template, cv2.TM_CCOEFF_NORMED)
             threshold = 0.99
             loc = np.where(res >= threshold)
+            # print(f'len:', loc[::-1])
 
             for pt in zip(*loc[::-1]):
-                cv2.rectangle(after, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 1)
+                cv2.rectangle(result_after, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 1)
+                cv2.line(result_after, (abs(leftAns + rightAns) // 2, abs(upperAns + bottomAns) // 2), (pt[0] + w // 2, pt[1] + h // 2), (0, 0, 0))
+
+            cv2.rectangle(result_after, (leftAns, upperAns), (rightAns, bottomAns), (255, 0, 0), 2)
+            if len(loc[0]) == 0:
+                cv2.rectangle(result_after, (leftAns, upperAns), (rightAns, bottomAns), (0, 0, 255), 2)
 
 
 
-cv2.imwrite('rectangle.png', after)
-
+cv2.imwrite('rectangle.png', result_after)
+# cv2.imshow('after', after)
+# cv2.imshow('before', before)
 
 cv2.waitKey(0)
