8899504624f27760ff0032cc6822a24a866a18cd
Yaroslav <yaroslavpopovich04@gmail.com>
2019-11-08 20:47:43 +0200

    +fields

--- main.py
+++ main.py
@@ -6,6 +6,97 @@ after = cv2.imread('2.png')
 
 result_after = after.copy()
 
+###############WORKING WITH IMAGES##############################################
+ask = input('Do you want to work with field?[Y/n]: ')
+if ask == 'Y' or ask == 'y':
+    print('changintg from bgr to hsv...\n')
+    before = cv2.cvtColor(before, cv2.COLOR_BGR2HSV)
+    after = cv2.cvtColor(after, cv2.COLOR_BGR2HSV)
+
+
+    before[:, :, 2] = 120
+    before[:, :, 1] = 120
+    after[:, :, 2] = 120
+    after[:, :, 1] = 120
+
+    print('bluring...')
+    # beps = 35
+    beps = 35
+    before = cv2.GaussianBlur(before, (beps, beps), 0)
+    after = cv2.GaussianBlur(after, (beps, beps), 0)
+
+    used = []
+    for i in range(0, before.shape[0]):
+        used.append([])
+        for j in range(0, before.shape[1]):
+            used[i].append(False)
+
+    def zones(y, x, val, eps, hsv):
+        global used
+        q = [(y, x)]
+        while len(q) > 0:
+            x = q[0][1]
+            y = q[0][0]
+            used[y][x] = True
+            # print(f'y: {y} x: {x} used[{y}][{x}]: {used[y][x]} val: {val}')
+            hsv[y][x][0] = val
+            if x + 1 < hsv.shape[1] and used[y][x + 1] == False and abs(float(hsv[y][x + 1][0]) - val) <= eps:
+                # print(abs(float(hsv[y][x + 1][0]) - val))
+                q.append((y, x + 1))
+                used[y][x + 1] = True
+            if x - 1 >= 0 and used[y][x - 1] == False and abs(float(hsv[y][x - 1][0]) - val) <= eps:
+                # print(abs(float(hsv[y][x - 1][0]) - val))
+                q.append((y, x - 1))
+                used[y][x - 1] = True
+
+            if y + 1 < hsv.shape[0] and used[y + 1][x] == False and abs(float(hsv[y + 1][x][0]) - val) <= eps:
+                # print(abs(float(hsv[y + 1][x][0]) - val))
+                q.append((y + 1, x))
+                used[y + 1][x] = True
+            if y - 1 >= 0 and used[y - 1][x] == False and abs(float(hsv[y - 1][x][0]) - val) <= eps:
+                # print(abs(float(hsv[y - 1][x][0]) - val))
+                q.append((y - 1, x))
+                used[y - 1][x] = True
+            del q[0]
+        return hsv
+
+
+    print('working with 1st photo...\nLoading...')
+    for i in range(0, before.shape[0]):
+        for j in range(0, before.shape[1]):
+            for g in range(1, 11):
+                if i == before.shape[0] // 100 * g * 10 and j == 0:
+                    print(g * 10, f'%')
+            if used[i][j] == False:
+                # print(f"y: {i} - x: {j}")
+                before = zones(i, j, before[i][j][0], 15.25, before)
+
+    used = []
+    for i in range(0, before.shape[0]):
+        used.append([])
+        for j in range(0, before.shape[1]):
+            used[i].append(False)
+
+    print('working with 2nd photo...\nLoading...')
+    for i in range(0, before.shape[0]):
+        for j in range(0, before.shape[1]):
+            for g in range(1, 11):
+                if i == before.shape[0] // 100 * g * 10 and j == 0:
+                    print(g * 10, f'%')
+            if used[i][j] == False:
+                # print(f"y: {i} - x: {j}")
+                after = zones(i, j, after[i][j][0], 15.25, after)
+
+    print('changing from hsv to bgr...')
+    before = cv2.cvtColor(before, cv2.COLOR_HSV2BGR)
+    after = cv2.cvtColor(after, cv2.COLOR_HSV2BGR)
+
+# cv2.imwrite('before_blur.png', before)
+# cv2.imwrite('after_blur.png', after)
+# exit(code=0)
+###############WORKING WITH IMAGES##############################################
+
+
 H = before.shape[0]
 W = before.shape[1]
 
@@ -132,9 +223,9 @@ print(f'{H} x {W}')
 print('loading...')
 for i in range(0, H):
     for j in range(0, W):
-        # for g in range(1, 11):
-        #     if i == H // 100 * g * 10 and j == 0:
-        #         print(g * 10, f'%')
+        for g in range(1, 11):
+            if i == H // 100 * g * 10 and j == 0:
+                print(g * 10, f'%')
         leftAns, rightAns, upperAns, bottomAns, col = (-1, -1, -1, -1, -1)
         if used[i][j] == False:
             leftAns, rightAns, upperAns, bottomAns, col = bfs2(i, j, maxSize)
@@ -158,13 +249,13 @@ for i in range(0, H):
 print('loading...')
 for i in range(0, H):
     for j in range(0, W):
-        # for g in range(1, 11):
-        #     if i == H // 100 * g * 10 and j == 0:
-        #         print(g * 10, f'%')
+        for g in range(1, 11):
+            if i == H // 100 * g * 10 and j == 0:
+                print(g * 10, f'%')
         leftAns, rightAns, upperAns, bottomAns, col = (-1, -1, -1, -1, -1)
         if used[i][j] == False:
             leftAns, rightAns, upperAns, bottomAns, col = bfs(i, j, maxSize)
-        if col > 9 and abs(leftAns - rightAns) > 2 and abs(upperAns - bottomAns) > 2:
+        if col > 40 and abs(leftAns - rightAns) > 2 and abs(upperAns - bottomAns) > 2:
             sample = before[upperAns:bottomAns, leftAns:rightAns]
             # if i > 2:
             #     cv2.imwrite(f'{i}.png', sample)
